5add034ffa5a6e77d09fde434f779d90ab7b5222
cap-sdebbich <safwen.debbichi@capgemini.com>
2021-03-18 19:27:22 +0100

    fixed

--- CI-Sample/AppDelegate.swift
+++ CI-Sample/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 //sqdsqqddd
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Overridqsdsqddsddsdpoint for customization after application launch.
+        // Overridqsdsqddsddsdqsdpoint for customization after application launch.
         return true
     }
 
