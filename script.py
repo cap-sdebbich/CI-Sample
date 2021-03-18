ad7b6341b4148469e4939e463ffa3b1689ca6f4b
cap-sdebbich <safwen.debbichi@capgemini.com>
2021-03-18 15:13:09 +0100

    fixed

--- script.py
+++ script.py
@@ -7,12 +7,13 @@ coverageFile = open('coverage.json')
 coverageJson = json.load(coverageFile)
 
 coveredFiles = coverageJson["targets"][0]["files"]
+coverage = -100
 if sourcePath in filePath:
     for covered in coveredFiles:
         path = covered["path"]
         if filePath in path:
             coverage = covered["lineCoverage"] * 100
-            print(coverage)
             break
 
+print(coverage)
 coverageFile.close()
