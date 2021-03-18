b5c8fc862b6772a8403014c93df62041d5c0085e
Safwen DEBBICHI <safwen.debbichi@capgemini.com>
2021-03-18 17:35:37 +0100

    dsqd

--- CI-Sample/AppDelegate.swift
+++ CI-Sample/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 //sqdsqqddd
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Overridqsdpoint for customization after application launch.
+        // Overridqsdsqdpoint for customization after application launch.
         return true
     }
 
